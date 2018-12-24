404dd0a918000e69805ea88168278b5c3c575ca4
tatumza <44038520+tatumza@users.noreply.github.com>
2018-12-24 21:21:17 +0700

    Update line.php

--- line.php
+++ line.php
@@ -2,7 +2,7 @@
   
 
 function send_LINE($msg){
- $access_token = 'qLip9omRdSnsaKFlsWmCCx9pdvAcRd1CGb6XfH/K3aKVgmHS4Eh/a35I8S1q8XVCZQVJUVIPa2B/c1ZJHfEyA8vUgqlUeIfqTkw607IKQ7yCasUHW34wj+CGzB6bOafYNDSGkh87GIr+Tns7fqFqVAdB04t89/1O/w1cDnyilFU='; 
+ $access_token = 'DuJHzRjH7aJrVgklISGJxpAo4qm3bRej0YDhgrfpcWC'; 
 
   $messages = [
         'type' => 'text',
